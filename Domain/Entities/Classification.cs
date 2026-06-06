346727c178818ff6802cb7a50d3277086ec44dcc
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-06 16:04:26 -0600

    feat(entities): entidades segun la estructura de la base de datos

---
+++ Domain/Entities/Classification.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Classification
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+    }
+}
---
+++ Domain/Entities/TicketAttachment.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public class TicketAttachment
+    {
+        public int Id { get; set; }
+
+        public int TicketId { get; set; }
+
+        public string? FileName { get; set; }
+
+        public required string FileUrl { get; set; }
+
+        public DateTime UploadedDate { get; set; }
+
+        public Ticket? Ticket { get; set; }
+    }
+}
