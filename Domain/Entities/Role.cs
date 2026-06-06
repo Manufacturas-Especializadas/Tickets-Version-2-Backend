e3a97598599d04afef20245b007c382f143abf2b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-06 16:09:21 -0600

    feat(entitie): propiedades de la tabla rol

--- Domain/Entities/Role.cs
+++ Domain/Entities/Role.cs
@@ -2,6 +2,10 @@
 {
     public class Role
     {
+        public int Id { get; set; }
 
+        public required string Name { get; set; }
+
+        public ICollection<User> Users { get; set; } = new List<User>();
     }
 }
