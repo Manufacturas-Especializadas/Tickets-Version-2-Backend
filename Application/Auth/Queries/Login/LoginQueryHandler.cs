971ac34914475753c046366b00c0b2c3f1b3cdf7
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 09:32:23 -0600

    feat(QueryHandler): metodo para el login del usuario

---
+++ Application/Auth/Queries/Login/LoginQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Auth.Queries.Login
+{
+    public class LoginQueryHandler(
+     IApplicationDbContext dbContext,
+     IJwtTokenGenerator jwtTokenGenerator,
+     IPasswordHasher passwordHasher) : IRequestHandler<LoginQuery, AuthResponse>
+    {
+        public async Task<AuthResponse> Handle(LoginQuery request, CancellationToken cancellationToken)
+        {
+            var user = await dbContext.Users
+                .Include(u => u.Role)
+                .FirstOrDefaultAsync(u => u.PayRollNumber == request.PayRollNumber, cancellationToken);
+
+            if (user is null || !passwordHasher.Verify(user.PasswordHash, request.Password))
+            {
+                throw new UnauthorizedAccessException("Credenciales inválidas.");
+            }
+
+            var token = jwtTokenGenerator.GenerateToken(user);
+
+            return new AuthResponse(
+                UserId: user.Id,
+                Name: user.Name,
+                Role: user.Role?.Name ?? "User",
+                Token: token
+            );
+        }
+    }
+}
