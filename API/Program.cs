54bb6fc8ecc2aef017ad4adf05777e4cc6fec1d3
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-06 20:03:56 -0600

    feat(program): servicios de la capa Application

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Application;
 using Infrastructure;
 using Microsoft.OpenApi;
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddSwaggerGen(c =>
