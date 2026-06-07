b98bb629bf878bab7b41fa88c22f73e6d0175c2a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 14:44:07 -0600

    feat(classifications): lista de clasificaciones

---
+++ API/Controllers/ClassificationsController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Tickets.Queries.GetClassifications;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassificationsController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet("getClassifications")]
+        public async Task<IActionResult> GetClassifications(CancellationToken cancellationToken)
+        {
+            var query = new GetClassificationsQuery();
+            var classifications = await mediator.Send(query, cancellationToken);
+
+            return Ok(classifications);
+        }
+    }
+}
