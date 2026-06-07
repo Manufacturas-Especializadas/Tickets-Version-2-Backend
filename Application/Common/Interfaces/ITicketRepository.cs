5ec5d9e6f1b7953eda0d1a44bc8285b821d40733
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-06 19:06:19 -0600

    feat(IRepository): interfaz para la creacion del ticket

---
+++ Application/Common/Interfaces/ITicketRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Interfaces
+{
+    public interface ITicketRepository
+    {
+        Task<int> AddAsync(Ticket ticket, CancellationToken cancellationToken);
+    }
+}
