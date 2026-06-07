73819746732bb6b297bc005410b9ca5500ff932b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 10:46:35 -0600

    feat(Commands): dto para crear un usuario

---
+++ Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.Users.Commands.CreateUser
+{
+    public record CreateUserCommand(
+        string Name,
+        int PayRollNumber,
+        int RolId,
+        string Password
+    ) : IRequest<int>;
+}
