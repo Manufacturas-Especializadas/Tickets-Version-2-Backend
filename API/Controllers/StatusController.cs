1ccb2368b74aafefadc719629bf293193f64c9fd
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 15:21:46 -0600

    feat(status): status

---
+++ API/Controllers/StatusController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Tickets.Queries.GetStatus;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet("getStatus")]
+        public async Task<IActionResult> GetStatus(CancellationToken cancellationToken)
+        {
+            var query = new GetStatusQuery();
+
+            var classifications = await mediator.Send(query, cancellationToken);
+
+            return Ok(classifications);
+        }
+    }
+}
---
+++ Application/Tickets/Queries/GetStatus/GetStatusQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Application.Tickets.Queries.GetStatus
+{
+    public record GetStatusQuery(): IRequest<List<StatusDto>>;
+}
