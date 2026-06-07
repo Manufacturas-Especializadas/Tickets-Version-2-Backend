6f16017c45a6f6d7aa400128d47cc48a4513c4f2
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 09:37:07 -0600

    feat(Password): hashear y verificar la contraseña

---
+++ Infrastructure/Authentication/PasswordHasher.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Interfaces;
+
+namespace Infrastructure.Authentication
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+
+        public bool Verify(string passwordHash, string inputPassword) => BCrypt.Net.BCrypt.Verify(inputPassword, passwordHash);
+    }
+}
