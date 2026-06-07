a02b2cd8aafb3eddba1e2f092321f16593b9b193
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 09:33:38 -0600

    feat(IRepository): interfaz para hashear la constraseña y verificar

---
+++ Application/Common/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Common.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+
+        bool Verify(string passwordHash, string inputPassword);
+    }
+}
