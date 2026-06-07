0db6853c84e56c7789da359c61d3e832c2438456
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 09:36:24 -0600

    feat(Authentication): generacion del token jwt

---
+++ Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.Authentication
+{
+    public class JwtTokenGenerator(IConfiguration configuration) : IJwtTokenGenerator
+    {
+        public string GenerateToken(User user)
+        {
+            var secretKey = configuration["JwtSettings:Secret"]!;
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Name, user.Name),
+            new Claim("payRollNumber", user.PayRollNumber.ToString()),
+            new Claim(ClaimTypes.Role, user.Role?.Name ?? "User")
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["JwtSettings:Issuer"],
+                audience: configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(double.Parse(configuration["JwtSettings:ExpiryMinutes"]!)),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
