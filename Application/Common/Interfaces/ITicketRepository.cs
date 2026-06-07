98c1ea30cea8d9d00463fb992b6af55caab8d243
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-06 20:00:42 -0600

    feat(IRepository): interfaz para obtener todos los tickets

--- Application/Common/Interfaces/ITicketRepository.cs
+++ Application/Common/Interfaces/ITicketRepository.cs
@@ -4,6 +4,8 @@ namespace Application.Common.Interfaces
 {
     public interface ITicketRepository
     {
+        Task<IEnumerable<Ticket>> GetAllWithDetailsAsync(CancellationToken cancellationToken);
+
         Task<int> AddAsync(Ticket ticket, CancellationToken cancellationToken);
     }
 }
