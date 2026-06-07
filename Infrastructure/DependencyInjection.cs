888d6255dc82fb488e63a29cb1b01e0f70328e12
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-06 19:45:43 -0600

    feat(injection): archivo para el registro de servicios

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+using Azure.Storage.Blobs;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repositories;
+using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("Connection")));
+
+            services.AddScoped<ITicketRepository, TicketRepository>();
+
+            var blobConnectionString = configuration.GetConnectionString("AzureBlobStorage");
+            services.AddSingleton(x => new BlobServiceClient(blobConnectionString));
+
+            services.AddScoped<IFileStorageService, AzureBlobStorageService>();
+
+            return services;
+        }
+    }
+}
