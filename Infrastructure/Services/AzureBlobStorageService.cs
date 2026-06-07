05821840bec0c9b2e443593ccb2a134e8f100ffa
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-06 19:45:17 -0600

    feat(service): metodo para el guardado de files en azure

---
+++ Infrastructure/Services/AzureBlobStorageService.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Interfaces;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace Infrastructure.Services
+{
+    public class AzureBlobStorageService : IFileStorageService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        private readonly string _containerName = "tickets";
+
+        public AzureBlobStorageService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public async Task<string> UploadFileAsync(FileUploadDto file, CancellationToken cancellationToken)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+            await containerClient.CreateIfNotExistsAsync(PublicAccessType.BlobContainer, cancellationToken: cancellationToken);
+
+            var uniqueFileName = $"{Guid.NewGuid()}-{file.FileName}";
+            var blobClient = containerClient.GetBlobClient(uniqueFileName);
+
+            var blobHttpHeaders = new BlobHttpHeaders { ContentType = file.ContentType };
+
+            await blobClient.UploadAsync(
+                file.Content,
+                new BlobUploadOptions { HttpHeaders = blobHttpHeaders },
+                cancellationToken
+            );
+
+            return blobClient.Uri.ToString();
+        }
+    }
+}
